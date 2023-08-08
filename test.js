77bee87b8644546796e407c4ad881518a3affc71
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-08-08 13:12:52 +0300

    save

---
+++ test.js
@@ -0,0 +1,4 @@
+function add(value1, value2) {
+    return value1 + value2;
+}
+console.log(add(1, 3));
---
+++ test.ts
@@ -0,0 +1,5 @@
+function add(value1: number, value2: number) {
+  return value1 + value2;
+}
+
+console.log(add(1, 3));
