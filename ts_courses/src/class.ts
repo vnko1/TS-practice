9e36a3fedfc826ac6d7eec1209f9acbc7bbc37d0
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-08-09 14:55:27 +0300

    save

--- ts_courses/src/class.ts
+++ ts_courses/src/class.ts
@@ -86,23 +86,124 @@ b.showProperty();
 
 // pilot.greet("Вас вітає капітан корабля");
 // pilot.flyMessage();
-abstract class Plane {
-  protected pilotInCabin = false;
+// abstract class Plane {
+//   protected pilotInCabin = false;
 
-  public sitInPlane() {
-    this.pilotInCabin = true;
-  }
+//   public sitInPlane() {
+//     this.pilotInCabin = true;
+//   }
 
-  public abstract startEngine(): boolean;
-}
+//   public abstract startEngine(): boolean;
+// }
 
-class Maize extends Plane {
-  public startEngine() {
-    // Запускаємо винти двигуна.
+// class Maize extends Plane {
+//   public startEngine() {
+//     // Запускаємо винти двигуна.
 
-    return true;
-  }
-}
+//     return true;
+//   }
+// }
 
-const plane = new Maize();
-console.log(plane.sitInPlane());
+// const plane = new Maize();
+// console.log(plane.sitInPlane());
+
+// class Captain {
+//   constructor(public name: string) {}
+// }
+
+// class Ship {
+//   constructor(private captain: Captain) {}
+
+//   getCaptain(): void {
+//     console.log(this.captain);
+//   }
+// }
+// const ship = new Ship(new Captain("Joe"));
+// ship.getCaptain();
+
+// class Captain {
+//   constructor(public name: string) {}
+// }
+
+// class Ship {
+//   //   private captain: Captain = { name: "" };
+//   private static captain: Captain;
+
+//   setCaptain(name: string) {
+//     Ship.captain = new Captain(name);
+//   }
+
+//   sink() {
+//     console.log(
+//       `Ship is sinking! Captain ${Ship.captain.name} goes down with the ship.`
+//     );
+//   }
+// }
+
+// const ship = new Ship();
+// ship.setCaptain("John");
+// ship.sink();
+// console.log(ship);
+
+// class App {
+//   private static instance: App;
+
+//   constructor() {
+//     if (!App.instance) {
+//       App.instance = this;
+//     }
+
+//     return App.instance;
+//   }
+
+//   // ...
+// }
+
+// class Car {
+//   constructor(
+//     public model: string,
+
+//     public year: number,
+
+//     public color: string
+//   ) {}
+// }
+
+// class CarBuilder {
+//   private model: string = "";
+
+//   private year: number = 0;
+
+//   private color: string = "";
+
+//   setModel(model: string): CarBuilder {
+//     this.model = model;
+
+//     return this;
+//   }
+
+//   setYear(year: number): CarBuilder {
+//     this.year = year;
+
+//     return this;
+//   }
+
+//   setColor(color: string): CarBuilder {
+//     this.color = color;
+
+//     return this;
+//   }
+
+//   build(): Car {
+//     return new Car(this.model, this.year, this.color);
+//   }
+// }
+
+// const builder = new CarBuilder();
+
+// const car = builder
+//   .setModel("Tesla Model S")
+//   .setYear(2023)
+//   .setColor("Red")
+//   .build();
+// console.log(car);
